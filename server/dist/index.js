6d6fa22426ec56c98682d29078802ca4419250f1
SOMKUVAR <ayushisomkuvar2001.as@gamail.com>
2023-11-09 16:09:34 +0530

    first commit

---
+++ server/dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const run_java_1 = __importDefault(require("./route/run-java"));
+const app = (0, express_1.default)();
+const port = 3001;
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, cors_1.default)());
+app.use("/", run_java_1.default);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ server/dist/route/run-java.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const child_process_1 = require("child_process");
+const router = (0, express_1.Router)();
+router.post('/run-java', (req, res) => {
+    const javaCode = req.body.code;
+    const inputData = req.body.inputData;
+    const dockerProcess = (0, child_process_1.spawn)('docker', [
+        'run',
+        '--rm',
+        '-e',
+        `JAVA_CODE=${javaCode}`,
+        '-e',
+        `INPUT_DATA=${inputData}`,
+        'java-code-containers'
+    ]);
+    let executionOutput = '';
+    dockerProcess.stdout.on('data', (data) => {
+        executionOutput += data;
+    });
+    dockerProcess.stderr.on('data', (data) => {
+        executionOutput += data;
+    });
+    dockerProcess.on('close', (code) => {
+        if (code === 0) {
+            // Java code executed successfully
+            res.send(`Java Output: ${executionOutput}`);
+        }
+        else if (code == 127) {
+            console.log(executionOutput);
+            res.status(500).send('Internal Server Error');
+        }
+        else {
+            res.send(`Java Execution Error: ${executionOutput}`);
+        }
+    });
+});
+router.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+});
+exports.default = router;
